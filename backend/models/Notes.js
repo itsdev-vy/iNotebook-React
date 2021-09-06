57ce9a13a7190f526b3080fb60d86e4b4ba83a9a
Vinay <itsvinayadav@gmail.com>
2021-09-06 17:00:32 +0530

    Creating Routes & Schema for Storing Notes & Users

---
+++ backend/models/Notes.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const notesSchema = new Schema({
+    title : { type : String, required: true},
+    description : { type : String, required: true},
+    tag : { type : String, default:"General"},
+    date : { type : date, default: Date.now}
+});
+
+module.exports = mongoose.model('notes',notesSchema )
