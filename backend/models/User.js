bc70c50941f3a24f71ece74bbd4c48eba2260e5f
Vinay <itsvinayadav@gmail.com>
2021-09-06 17:47:20 +0530

    Storing Data into the Database Using Mongoose Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name : { type : String, required: true},
     email : { type : String, required: true, unique: true},
     password : { type : String, required: true},
-    date : { type : date, default: Date.now}
+    date : { type : Date, default: Date.now}
 });
 
 module.exports = mongoose.model('user',UserSchema )
