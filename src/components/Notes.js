593eb1599e0778ff95a887c6185a23f184073979
Vinay <itsvinayadav@gmail.com>
2021-09-11 17:57:41 +0530

    Adding AddNote component to iNotebook

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,21 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
+import AddNote from "./AddNote";
 import Noteitem from "./Noteitem";
 
 function Notes() {
   const context = useContext(noteContext);
-  const { notes, setNotes } = context;
+  const { notes, addNote } = context;
   return (
-    <div className="row my-3">
-      <h1>Your Notes</h1>
-      {notes.map((note) => {
-        return <Noteitem key={note._id} note={note} />;
-      })}
-    </div>
+    <>
+      <AddNote />
+      <div className="row my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
   );
 }
 
